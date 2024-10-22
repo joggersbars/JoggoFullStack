d72175e817bd9dd2f642c6fcef5e1b54b36e5d56
Vittem <joaovitorjosedasilva0@gmail.com>
2024-10-22 21:01:33 +0200

    Modificaciones en /yonunca_intro e integracion de funcionalidades

--- Joggo/JoggoGamesFrontend/pruebas/yonunca_intro.js
+++ Joggo/JoggoGamesFrontend/pruebas/yonunca_intro.js
@@ -1,28 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Obtener el ID de la partida desde la URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const idPartida = urlParams.get('id_partida');
 
-    console.log("ID de la partida obtenido:", idPartida);
-
-    // Mostrar el ID de la partida en el HTML
-    const idPartidaElement = document.getElementById('id_partida');
-    if (idPartida) {
-        idPartidaElement.textContent = idPartida;
-    } else {
-        console.error("ID de partida no encontrado en la URL.");
-    }
-
-    // Generar el código QR si el ID de la partida está disponible
-    if (idPartida) {
-        console.log("Generando QR con la URL: http://localhost:5000/partida?id_partida=" + idPartida);
-        const qr = new QRious({
-            element: document.getElementById('qrPartida'),
-            value: `http://localhost:5000/partida?id_partida=${idPartida}`, // URL con el ID de la partida
-            size: 250,
-            backgroundAlpha: 0, // fondo transparente
-            foreground: '#000',  // color del código QR
-            level: 'H'  // nivel de corrección de errores
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        // Hacer una solicitud GET al servidor para obtener el ID de la partida
+        const response = await fetch('http://localhost:5000/crear_partida?game_name=Yo%20Nunca', { 
+            method: 'GET',
         });
+
+        if (response.ok) {
+            const result = await response.json();
+            const idPartida = result.id_partida;  // Obtener el ID de la partida del backend
+            console.log('ID de la partida recibido:', idPartida);
+
+            // Mostrar el ID de la partida en el HTML
+            document.getElementById('id_partida').textContent = idPartida;
+
+            // Generar el código QR con el ID de la partida
+            const qr = new QRious({
+                element: document.getElementById('qrPartida'),
+                value: `http://localhost:8001/partida_${idPartida}`, // URL con el ID de la partida
+                size: 250,
+                backgroundAlpha: 0, // Fondo transparente
+                foreground: '#000',  // Color del QR
+                level: 'H'  // Nivel de corrección de errores
+            });
+        } else {
+            console.error('Error al obtener el ID de la partida:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error al conectar con el servidor:', error);
     }
 });
+
+
+
+
+
+
+
+
