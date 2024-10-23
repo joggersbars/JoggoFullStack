f1540de243eda1178aae55e138ff1b45b1a8cc63
FranSC16 <106516846+FranSC16@users.noreply.github.com>
2024-10-23 18:56:01 +0200

    Corrigiendo algunos errores en crud.py

--- Joggo/JoggoGamesBackend/app/util/model.py
+++ Joggo/JoggoGamesBackend/app/util/model.py
@@ -2,13 +2,15 @@ from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database.database_configuration import Base
 
+STRING_LENGTH = 30
+
 # Tabla usuario cliente
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True) # Id del usuario
-    username = Column(String(30), index=True, unique=True) # Username del usuario
-    password = Column(String(30), index=True) # Contraseña del usuario
+    username = Column(String(STRING_LENGTH), index=True, unique=True) # Username del usuario
+    password = Column(String(STRING_LENGTH), index=True) # Contraseña del usuario
 
     # Relación con las partidas
     games = relationship("GamePlayer", back_populates="user") #
@@ -18,7 +20,7 @@ class Game(Base):
     __tablename__ = 'games'
 
     id = Column(Integer, primary_key=True, index=True) # Id de la partida
-    game_name = Column(String)
+    game_name = Column(String(STRING_LENGTH))
     num_players = Column(Integer)
 
     # Relación con jugadores conectados y frases
@@ -32,7 +34,7 @@ class GamePlayer(Base):
     id = Column(Integer, primary_key=True, index=True)
     game_id = Column(Integer, ForeignKey('games.id'))
     user_id = Column(Integer, ForeignKey('users.id'))
-    phrase = Column(String)  # Frase que introduce el jugador
+    phrase = Column(String(STRING_LENGTH))  # Frase que introduce el jugador
 
     # Relación con las tablas de juegos y usuarios
     game = relationship("Game", back_populates="players")
@@ -43,7 +45,7 @@ class GamePhrase(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     game_id = Column(Integer, ForeignKey('games.id'))
-    phrase = Column(String)
+    phrase = Column(String(STRING_LENGTH))
     
     # Relación con las respuestas de los jugadores
     responses = relationship("GameResponse", back_populates="phrase")
@@ -55,7 +57,7 @@ class GameResponse(Base):
     id = Column(Integer, primary_key=True, index=True)
     game_phrase_id = Column(Integer, ForeignKey('game_phrases.id'))
     player_id = Column(Integer, ForeignKey('users.id'))
-    response = Column(String)  # Ejemplo: "He hecho" o "No he hecho"
+    response = Column(String(STRING_LENGTH))  # Ejemplo: "He hecho" o "No he hecho"
 
     # Relación con frase y usuario
     phrase = relationship("GamePhrase", back_populates="responses")
