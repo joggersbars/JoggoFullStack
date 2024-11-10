c2dc2fe8941c19e5591785388bbb2e1ab59eefda
FranSC16 <106516846+FranSC16@users.noreply.github.com>
2024-11-10 19:04:17 +0100

    Ajustes finales

--- Joggo/JoggoGamesBackend/Run.py
+++ Joggo/JoggoGamesBackend/Run.py
@@ -1,4 +1,4 @@
 import uvicorn
 #Running server
 if __name__ == '__main__':
-    uvicorn.run("app.main:app",host="152.42.129.144" ,log_level="info",port=8002 ,reload=False)
+    uvicorn.run("app.main:app",host="0.0.0.0" ,log_level="info",port=8002 ,reload=False)
