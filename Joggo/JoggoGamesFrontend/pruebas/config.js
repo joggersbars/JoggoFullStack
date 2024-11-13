f93439559f974b2d0e2d37c88efd6bdb3d3abd47
FranSC16 <106516846+FranSC16@users.noreply.github.com>
2024-11-13 22:26:35 +0100

    No se puede con localhost

--- Joggo/JoggoGamesFrontend/pruebas/config.js
+++ Joggo/JoggoGamesFrontend/pruebas/config.js
@@ -1,5 +1,4 @@
 // config.js
-window.API_URL = "http://localhost:8002";
+window.API_URL = "http://backend:8002";
 //window.FRONTEND_URL =  "http://152.42.129.144:8001";
-window.FRONTEND_URL =  "http://localhost:8001";
-// window.FRONTEND_URL =  "http://joggo.es";
+window.FRONTEND_URL =  "http://joggo.es";
