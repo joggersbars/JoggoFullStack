8a21cb9090fe495eb8459b2931ba645ab66d72a1
FranSC16 <106516846+FranSC16@users.noreply.github.com>
2024-11-13 22:30:15 +0100

    Ajustes de control

--- Joggo/JoggoGamesFrontend/pruebas/config.js
+++ Joggo/JoggoGamesFrontend/pruebas/config.js
@@ -1,4 +1,4 @@
 // config.js
-window.API_URL = "http://backend:8002";
+window.API_URL = "http://152.42.129.144:8002";
 //window.FRONTEND_URL =  "http://152.42.129.144:8001";
 window.FRONTEND_URL =  "http://joggo.es";
