1ddc410747b0c8b8ea4db14d7cb930ef028e9cc2
joggersbars <joggobares@gmail.com>
2024-11-14 20:33:26 +0100

    Update config.js

--- Joggo/JoggoGamesFrontend/pruebas/config.js
+++ Joggo/JoggoGamesFrontend/pruebas/config.js
@@ -1,9 +1,5 @@
 // config.js
 
-//window.API_URL = "http://152.42.129.144:8002";
+window.API_URL = "http://152.42.129.144:8002";
 //window.FRONTEND_URL =  "http://152.42.129.144:8001";
-//window.FRONTEND_URL =  "http://joggo.es";
-
-//Configuracion para realizar pruebas en MI LOCAL
-window.API_URL =  "http://localhost:8002";
-window.FRONTEND_URL =  "http://localhost:8001";
+window.FRONTEND_URL =  "http://joggo.es";
