0af0f7ca4c63bb203f918bdbf3960b28351becdb
FranSC16 <106516846+FranSC16@users.noreply.github.com>
2024-11-15 21:45:33 +0100

    Cambiando puertos

--- Joggo/JoggoGamesFrontend/pruebas/config.js
+++ Joggo/JoggoGamesFrontend/pruebas/config.js
@@ -2,4 +2,4 @@
 
 window.API_URL = "https://joggo.es:65000";
 //window.FRONTEND_URL =  "http://152.42.129.144:8001";
-window.FRONTEND_URL =  "https://joggo.es:64000";
+window.FRONTEND_URL =  "https://joggo.es:443";
