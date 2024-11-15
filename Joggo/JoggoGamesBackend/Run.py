af32050a3736286da6dccd4bba868a4c3af16458
FranSC16 <106516846+FranSC16@users.noreply.github.com>
2024-11-15 20:50:08 +0100

    Actualizando logging a modo debug

--- Joggo/JoggoGamesBackend/Run.py
+++ Joggo/JoggoGamesBackend/Run.py
@@ -1,4 +1,4 @@
 import uvicorn
 #Running server
 if __name__ == '__main__':
-    uvicorn.run("app.main:app",host="0.0.0.0" ,log_level="info",port=8002 ,reload=False)
+    uvicorn.run("app.main:app",host="0.0.0.0" ,log_level="debug",port=8002 ,reload=False)
