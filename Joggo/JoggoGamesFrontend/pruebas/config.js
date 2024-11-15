ed459b4f0ccf62dc706a7736d671a3a418521cf6
FranSC16 <106516846+FranSC16@users.noreply.github.com>
2024-11-15 20:39:36 +0100

    Ajustes para https

--- Joggo/JoggoGamesFrontend/pruebas/config.js
+++ Joggo/JoggoGamesFrontend/pruebas/config.js
@@ -1,5 +1,5 @@
 // config.js
 
-window.API_URL = "http://152.42.129.144:8002";
+window.API_URL = "https://152.42.129.144:8002";
 //window.FRONTEND_URL =  "http://152.42.129.144:8001";
-window.FRONTEND_URL =  "http://joggo.es";
+window.FRONTEND_URL =  "https://joggo.es";
